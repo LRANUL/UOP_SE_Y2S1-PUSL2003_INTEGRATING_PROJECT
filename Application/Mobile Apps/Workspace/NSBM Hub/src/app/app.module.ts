dcf6e885a180f4c79d3c36d8be1e6aead538e128
Ranul L <rpladduwahetty@students.nsbm.lk>
2020-02-05 02:43:54 +0530

    W6
    * Added App Icons
    * Added Loading Windows
    * Added Network Services
    * Removed Redantant Code

--- Application/Mobile Apps/Workspace/NSBM Hub/src/app/app.module.ts
+++ Application/Mobile Apps/Workspace/NSBM Hub/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HttpClientModule } from "@angular/common/http";
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -12,7 +12,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
