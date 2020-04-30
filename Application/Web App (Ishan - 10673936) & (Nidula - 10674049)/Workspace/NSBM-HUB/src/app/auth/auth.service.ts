b6443cb38b240a1525b4399378abed0921dac61a
nnwijesuriya <51411336+nnwijesuriya@users.noreply.github.com>
2020-04-30 23:23:20 +0530

    W18U16

--- Application/Web App (Ishan - 10673936) & (Nidula - 10674049)/Workspace/NSBM-HUB/src/app/auth/auth.service.ts
+++ Application/Web App (Ishan - 10673936) & (Nidula - 10674049)/Workspace/NSBM-HUB/src/app/auth/auth.service.ts
@@ -33,4 +33,33 @@ export class AuthService {
         }
       })
   }
+
+  createUser(user){
+    this.afAuth.createUserWithEmailAndPassword( user.email, user.password)
+      .then( userCredential => {
+        this.newUser = user;
+        userCredential.user.updateProfile( {
+          displayName: user.firstName + ' ' + user.lastName + '' + user.ids + '' + user.faculty + '' + user.degreeCode + '' + user.degreeProgram + '' + user.university
+        });
+        this.insertUserData(userCredential)
+          .then(() => {
+            this.route.navigate(['login']);
+          });
+      })
+      .catch( error => {
+        this.eventAuthError.next(error);
+      });
+  }
+  insertUserData(userCredential: firebase.auth.UserCredential) {
+    return this.db.doc(`Users/${userCredential.user.email}`).set({
+      email: this.newUser.email,
+      firstName: this.newUser.firstName,
+      lastName: this.newUser.lastName,
+      studentID: this.newUser.ids,
+      faculty: this.newUser.faculty,
+      degreeCode: this.newUser.degreeCode,
+      degreeProgram: this.newUser.degreeProgram,
+      awardinguniversityBody: this.newUser.university
+    })
+  }
 }
